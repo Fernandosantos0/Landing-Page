81b5b9e4a5636382698b980688f3ffe303d6a1ec
Fernando Santos Pereira <92190031+Fernandosantos0@users.noreply.github.com>
2022-07-05 16:37:40 -0300

    v7

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 const menuMobile = window.document.getElementById('btnMenu');
 
 const abrirMenuMobile = () => {
+    const mobile = window.document.getElementById('menu-mobile');
+    
+    if(mobile.style.display === 'block') {
+        mobile.style.display = 'none';
+        menuMobile.src="./img/open-menu.svg";
+    } else {
+        mobile.style.display = 'block';
+        menuMobile.src="./img/close-menu.svg";
+    }
+
 };
 
 menuMobile.addEventListener('click',abrirMenuMobile);
